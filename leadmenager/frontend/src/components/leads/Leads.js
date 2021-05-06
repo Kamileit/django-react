eabe1fa208812a09db6bd43c05d442992f9da1b0
Kamil <kamilmaludzinski@gmail.com>
2021-05-06 11:17:17 +0200

    git commit redux

--- leadmenager/frontend/src/components/leads/Leads.js
+++ leadmenager/frontend/src/components/leads/Leads.js
@@ -1,16 +1,31 @@
-import React,{Component} from "react";
+import React,{Component,Fragment} from "react";
+import {connect} from "react-redux";
+import PropTypes from 'prop-types';
+import {getLeads} from "../../actions/leads";
 
 
 export class Leads extends Component {
+    static propTypes = {
+        leads: PropTypes.array.isRequired
+    }
+    componentDidMount() {
+        this.props.getLeads();
+    }
+
 
     render() {
         return(
+
             <div>
                 <h1>Leads List</h1>
             </div>
-        )
+
+        );
 
     }
 }
 
-export default Leads
+const mapStateToProps = state => ({
+   leads: state.leads.leads
+});
+export default connect(mapStateToProps,{getLeads})(Leads);
